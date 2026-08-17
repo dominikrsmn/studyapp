a6c1c5ac751404ac40d167a0608fa0f48f6543dc
Dominik Ruschmann <dominik.rsmn@gmail.com>
2026-08-17 18:10:17 +0200

    fix imports

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -7,8 +7,8 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './auth/auth.interceptor';
-import { AuthTokenService } from './auth/auth-token.service';
+import { authInterceptor } from './core/auth/auth.interceptor';
+import { AuthTokenService } from './core/auth/auth-token.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Route } from '@angular/router';
-import { MagicLinkVerificationComponent } from './auth/magic-link-verification.component';
-import { HomeComponent } from './home/home.component';
+import { MagicLinkVerificationComponent } from './core/auth/magic-link-verification.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     pathMatch: 'full',
-    component: HomeComponent,
+    //    component: HomeComponent,
   },
   {
     path: 'auth/verify',
