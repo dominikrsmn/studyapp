8644021b866ec094f8a507862d3b8fb7ec620f99
Dominik Ruschmann <dominik.rsmn@gmail.com>
2026-08-20 12:20:50 +0200

    implement sse auth service

---
+++ apps/web/src/app/core/auth/sse-auth.service.ts
@@ -0,0 +1,57 @@
+import { inject, Injectable } from '@angular/core';
+import { AuthTokenService } from './auth-token.service';
+import { Observable, Subscriber } from 'rxjs';
+import {
+  EventSourceMessage,
+  fetchEventSource,
+} from '@microsoft/fetch-event-source';
+
+@Injectable({ providedIn: 'root' })
+export class SseAuthService {
+  private readonly authTokenService = inject(AuthTokenService);
+
+  connect<T>(url: string): Observable<T> {
+    return new Observable<T>((subscriber: Subscriber<T>) => {
+      const controller = new AbortController();
+
+      void this.connectInternal(url, controller, (message) => {
+        try {
+          subscriber.next(JSON.parse(message.data) as T);
+        } catch (error) {
+          subscriber.error(error);
+        }
+      }).catch((error) => {
+        if (!controller.signal.aborted) {
+          subscriber.error(error);
+        }
+      });
+      return () => {
+        controller.abort();
+      };
+    });
+  }
+
+  private async connectInternal(
+    url: string,
+    controller: AbortController,
+    onMessage: (message: EventSourceMessage) => void,
+  ): Promise<void> {
+    await fetchEventSource(url, {
+      signal: controller.signal,
+
+      headers: {
+        Authorization: `Bearer ${this.authTokenService.getAccessToken()}`,
+      },
+
+      onmessage: onMessage,
+
+      onopen: async (response: Response) => {
+        if (response.ok) return;
+        if (response.status === 401) throw new UnauthorizedSseError();
+        throw new Error('SSE connection failed, status: ' + response.status);
+      },
+    });
+  }
+}
+
+class UnauthorizedSseError extends Error {}
