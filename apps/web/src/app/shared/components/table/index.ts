1e6682214e3222c3dc6e756b308879c50aa5a796
Dominik Ruschmann <dominik.rsmn@gmail.com>
2026-08-20 10:23:58 +0200

    add zardui table

---
+++ apps/web/src/app/shared/components/table/index.ts
@@ -0,0 +1,3 @@
+export * from '@study/shared/components/table/table.component';
+export * from '@study/shared/components/table/table.imports';
+export * from '@study/shared/components/table/table.variants';
