9b3b41e238102cf1b80c1d39e32d5cab07fd8928
Dominik Ruschmann <dominik.rsmn@gmail.com>
2026-09-01 12:44:26 +0200

    db migration for removing document off db

---
+++ apps/backend/prisma/migrations/20260901103031_remove_document_from_db/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `document` on the `Source` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Source" DROP COLUMN "document";
